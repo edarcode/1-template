ecde381ab9ca0faa6ffd4d0df13b7a976bd640cb
Edwin Ortiz <edarcode@gmail.com>
2024-07-01 11:34:14 -0500

    refactor

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,5 +1,10 @@
+import Counter from "../components/Counter/Counter";
 import css from "./css.module.css";
 
 export default function App() {
-  return <div className={css.app}>😎</div>;
+  return (
+    <div className={css.app}>
+      <Counter />
+    </div>
+  );
 }
