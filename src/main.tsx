17c3abd384022864efbd2e5b31dd018f2951794e
Edwin Ortiz <edarcode@gmail.com>
2024-07-01 22:38:52 -0500

    refactor

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
-import { router } from "./Routes/router";
 import "./global.css";
+import { router } from "./router/router";
 
 ReactDOM.createRoot(document.querySelector("#root")!).render(
   <RouterProvider router={router} />
